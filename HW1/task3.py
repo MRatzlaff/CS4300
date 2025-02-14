a7a7b9db3ba3fd2839273751d968b52e07e733d4
Maddison Ratzlaff <mratzlaf@uccs.edu>
2025-02-14 01:47:01 +0000

    work on task 3

--- HW1/task3.py
+++ HW1/task3.py
@@ -11,9 +11,20 @@ def what_is_it(num):
     return result
 
 def prime_nums():
+    primes_list = []
     count = 0
     sieve = 2
     count_max = 10
 
-    while count > count_max:
-        for sieve 
+    # start at 2 because there are no prime numbers lower than 2
+    while count < count_max:
+        for i in range(2, count_max):
+            if(i%sieve != 0):
+                primes_list.append(i)
+                sieve += 1
+                
+            count_max += 1
+
+    print(primes_list)
+
+prime_nums()
