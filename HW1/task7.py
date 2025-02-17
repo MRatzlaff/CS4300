83b750eadf244671f75b2eb9cbc7f29d481ac64e
Maddison Ratzlaff <mratzlaf@uccs.edu>
2025-02-17 02:09:20 +0000

    finished

---
+++ HW1/task7.py
@@ -0,0 +1,17 @@
+# task 7
+import numpy as np 
+# demonstrate use of numpy
+
+#add
+def add_with_numpy(n1, n2):
+    return np.add(n1, n2)
+
+
+#subtract
+def sub_with_numpy(n1, n2):
+    return np.subtract(n1, n2)
+
+
+#multiply
+def mult_with_numpy(n1, n2):
+    return np.multiply(n1, n2)
