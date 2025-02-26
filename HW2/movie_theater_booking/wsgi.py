4d95c976c25e5b8997ff4178ee44c9c4987a4102
Maddison Ratzlaff <mratzlaf@uccs.edu>
2025-02-26 00:17:36 +0000

    restarted to build correctly

--- HW2/movie_theater_booking/wsgi.py
+++ HW2/movie_theater_booking/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bookings project.
+WSGI config for movie_theater_booking project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_theater_booking.settings')
 
 application = get_wsgi_application()
