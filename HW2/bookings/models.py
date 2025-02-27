bc33472a887b651671ab3e1f5da9f79912aa7e46
Maddison Ratzlaff <mratzlaf@uccs.edu>
2025-02-27 04:08:00 +0000

    cont hw2

--- HW2/bookings/models.py
+++ HW2/bookings/models.py
@@ -4,16 +4,23 @@ from django.db import models
 class Movie(models.Model):
     def __str__(self):
         return self.movie_title
-    movie_title = "movie title"
+    title = "movie title"
+    description = "description"
+    release_date = models.DateTimeField("release date")
+    duration = models.IntegerField(default=0)
 
 
 class Seat(models.Model):
     def __str__(self):
         return self.seats
-    seats = "seats available"
+    seat_number = models.IntegerField(default=0)
+    booking_status = models.CharField(max_length=50)
 
 
 class Booking(models.Model):
     def __str__(self):
         return self.current_bookings
-    current_bookings = "test bookings"
+    movie_chosen = models.CharField("movie", max_length=200)
+    seat_chosen = models.CharField("seat", max_length=10)
+    user = models.CharField("user", max_length=200)
+    booking_date = models.DateTimeField("date of movie", default=0)
