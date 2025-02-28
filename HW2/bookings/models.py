a4811fac375f53e557b82058d26c1e305e835c99
Maddison Ratzlaff <mratzlaf@uccs.edu>
2025-02-28 00:51:37 +0000

    cont hw2

--- HW2/bookings/models.py
+++ HW2/bookings/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Movie(models.Model):
     def __str__(self):
         return self.movie_title
-    title = "movie title"
-    description = "description"
-    release_date = models.DateTimeField("release date")
+    title = models.CharField(max_length=200, default='')
+    description = models.CharField(max_length=200, default='')
+    release_date = models.DateTimeField(default=datetime.now)
     duration = models.IntegerField(default=0)
 
 
@@ -14,13 +15,13 @@ class Seat(models.Model):
     def __str__(self):
         return self.seats
     seat_number = models.IntegerField(default=0)
-    booking_status = models.CharField(max_length=50)
+    booking_status = models.CharField(max_length=50, default='')
 
 
 class Booking(models.Model):
     def __str__(self):
         return self.current_bookings
-    movie_chosen = models.CharField("movie", max_length=200)
-    seat_chosen = models.CharField("seat", max_length=10)
-    user = models.CharField("user", max_length=200)
-    booking_date = models.DateTimeField("date of movie", default=0)
+    movie_chosen = models.CharField(max_length=200, default='')
+    seat_chosen = models.CharField(max_length=10, default='')
+    user = models.CharField(max_length=200, default='')
+    booking_date = models.DateTimeField(default=datetime.now)
--- HW2/bookings/views.py
+++ HW2/bookings/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.request import Request
+from .models import Movie
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the bookings page.")
+class MovieViewSet(viewsets.ViewSet):
+    def list(self,request:Request):
+        queryset=Movie.objects.all()
+        
